cd1fa56a8dda6dc0aa642594ddb1df794b80bbc2
Bariq Firjatullah <bariqfirjatullah1803@gmail.com>
2024-05-18 04:53:36 +0700

    update

---
+++ completion.py
@@ -0,0 +1,73 @@
+import requests
+import json
+import concurrent.futures
+import time
+
+
+def check(email):
+    url = "https://api.karismagarudamulia.com/api/v1/dev/update-completion"
+
+    split_data = email.split("|")
+
+    payload = json.dumps({
+        "email": str(split_data[0]),
+        "redeem_code": str(split_data[1]),
+        "post_test": int(split_data[2]),
+        "pre_test": int(split_data[3]),
+        "skill_test": int(split_data[4]),
+    })
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    try:
+        response_json = response.json()
+        return response_json
+    except json.JSONDecodeError:
+        return {"error": "Invalid JSON response"}
+
+
+def main():
+    # Prompt the user for the filename
+    filename = input("Please enter the filename: ")
+
+    try:
+        # Read emails from the file
+        with open(filename, 'r') as file:
+            emails = [line.strip() for line in file]
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+        return
+
+    total_emails = len(emails)
+    results = []
+    r1 = open('result.txt', 'a')
+
+    # Use ThreadPoolExecutor to send requests concurrently
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        start_time = time.time()
+        futures = {executor.submit(check, email): email for email in emails}
+        for count, future in enumerate(concurrent.futures.as_completed(futures), 1):
+            email = futures[future]
+            try:
+                data = future.result()
+                if data['success']:
+                    with open('completion.txt', 'a') as log:
+                        log.write(f'{email}\n')
+                end_time = time.time()
+                execution_time = end_time - start_time
+                print(f"{count}/{total_emails} - {email}: {execution_time:.2f} seconds")
+            except requests.RequestException as e:
+                print(f"{count}/{total_emails} - {email} 'error': RequestException: {e}")
+            except json.JSONDecodeError as e:
+                print(f"{count}/{total_emails} - {email} 'error': Invalid JSON response: {e}")
+            except Exception as e:
+                print(f"{count}/{total_emails} - {email} 'error': ", e)
+
+    r1.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ log.py
@@ -0,0 +1,77 @@
+import requests
+import json
+import concurrent.futures
+import time
+
+
+def check(email):
+    url = "https://api.karismagarudamulia.com/api/v1/dev/check-log-material"
+
+    payload = json.dumps({
+        "email": email
+    })
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    try:
+        response_json = response.json()
+        return response_json
+    except json.JSONDecodeError:
+        return {"error": "Invalid JSON response"}
+
+
+def main():
+    # Prompt the user for the filename
+    filename = input("Please enter the filename: ")
+
+    try:
+        # Read emails from the file
+        with open(filename, 'r') as file:
+            emails = [line.strip() for line in file]
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+        return
+
+    total_emails = len(emails)
+    results = []
+    r1 = open('result.txt', 'a')
+
+    # Use ThreadPoolExecutor to send requests concurrently
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        start_time = time.time()
+        futures = {executor.submit(check, email): email for email in emails}
+        for count, future in enumerate(concurrent.futures.as_completed(futures), 1):
+            email = futures[future]
+            try:
+                data = future.result()
+                if data['success']:
+                    voucher = data['voucher']
+                    redeem_code = data['redeem_code']
+                    data = data['data']
+                    with open('log.txt', 'a') as log:
+                        log.write(f'{email} | {voucher} | {redeem_code} | {data}\n')
+                # if data['success'] and data['code'] == 200:
+                #     emails.remove(email)
+                end_time = time.time()
+                execution_time = end_time - start_time
+                print(f"{count}/{total_emails} - {email}: {execution_time:.2f} seconds")
+                # print(json.dumps(data, indent=2))
+                # r1.write(f"{email}: {data['message']}\n")
+                # with open(filename, 'w') as file:
+                #     for email in emails:
+                #         file.write(f"{email}\n")
+            except requests.RequestException as e:
+                print(f"{count}/{total_emails} - {email} 'error': RequestException: {e}")
+            except json.JSONDecodeError as e:
+                print(f"{count}/{total_emails} - {email} 'error': Invalid JSON response: {e}")
+            except Exception as e:
+                print(f"{count}/{total_emails} - {email} 'error': ", e)
+
+    r1.close()
+
+
+if __name__ == "__main__":
+    main()
